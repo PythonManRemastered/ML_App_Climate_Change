e15dec27afb4048e682f73b8915c8704e301316a
PythonManRemastered <109542449+PythonManRemastered@users.noreply.github.com>
2024-05-25 20:35:58 +0530

    Update tabs.py
    integration of YOLO model with streamlit code

--- tabs.py
+++ tabs.py
@@ -74,3 +74,21 @@ with tab3:
 
     st.map(df)
 
+with tab4:
+    st.title("Image classifier 9000")
+    @st.cache_resource
+    def load_model():
+        mod = YOLO("best.pt")
+        return mod
+    
+    img = st.file_uploader("Upload an image to classify", type=["jpg", "png", "jpeg"])
+    
+    if img is not None:
+        img = Image.open(img)
+        st.image(img)
+        mod1 = load_model()
+        res = mod1.predict(img)
+        pred = res[0].probes.top5
+        for ind in pred:
+            st.write(res[0].names[ind])
+
