9b2eb92cd3b831bee4255ff5d2a12b75f9669369
PythonManRemastered <109542449+PythonManRemastered@users.noreply.github.com>
2024-05-27 11:25:04 +0530

    Update tabs.py

--- tabs.py
+++ tabs.py
@@ -45,10 +45,13 @@ with tab1:
 with tab2:
     st.title("Into the Stratosphere: Exploring Ozone Depletion and Atmospheric Dynamics")
     st.subheader("What is stratopheric ozone?")
-    st.markdown("" <style>
-                <strong>This text is important!</strong>
-                </style>"", unsafe_HTML_allow = True)
-                
+    st.markdown("""
+                - The atmosphere is divided into several layers; The two we will discuss here are: <b> the Troposphere, and the Stratosphere </b>
+                - Tropospheric ozone is primarily caused by car exhaust emissions during traffic, which produce nitrogen oxides. <b> The development of tropospheric ozone is hzardous to humans </b>
+                - Stratospheric ozone is naturally produced when solar radiation breaks down oxygen molecules in the atmosphere. <b> Stratospheric ozone protects us from UV radiation and makes the Earth habitable </b>
+                """                
+
+)
     
 
 with tab3:
