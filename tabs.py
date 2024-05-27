15da1cdda0d5b43930f815ebca9bb0ac50935457
PythonManRemastered <109542449+PythonManRemastered@users.noreply.github.com>
2024-05-27 15:24:32 +0530

    Update tabs.py

--- tabs.py
+++ tabs.py
@@ -99,7 +99,7 @@ with tab4:
     expander = st.expander("See explanation")
     expander.write('''
         The chart above shows some numbers I picked for you.
-        I rolled actual dice for these, so they're *guaranteed* to
+        I rolled actual dice for these, so they're **guaranteed** to
         be random.
     ''')
     expander.image("https://static.streamlit.io/examples/dice.jpg")
