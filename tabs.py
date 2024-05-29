8d1acfb05f84ac203a213cdb1a06808495166051
PythonManRemastered <109542449+PythonManRemastered@users.noreply.github.com>
2024-05-29 14:43:44 +0530

    Update tabs.py

--- tabs.py
+++ tabs.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 from ultralytics import YOLO
 from PIL import Image
-import plotly.figure_factory as px
+import pydeck as pdk
 
 
 st.set_page_config(layout="wide")
@@ -63,23 +63,39 @@ with tab2:
                 """                
 
 )
+       
+    chart_data = pd.DataFrame(
+       np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+       columns=['lat', 'lon'])
     
-
-    df = px.data.gapminder()
-
-    fig = px.scatter(
-        df.query("year==2007"),
-        x="gdpPercap",
-        y="lifeExp",
-        size="pop",
-        color="continent",
-        hover_name="country",
-        log_x=True,
-        size_max=60,
-)
-
-    tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
-    st.plotly_chart(fig, theme=None, use_container_width=True)
+    st.pydeck_chart(pdk.Deck(
+        map_style=None,
+        initial_view_state=pdk.ViewState(
+            latitude=37.76,
+            longitude=-122.4,
+            zoom=11,
+            pitch=50,
+        ),
+        layers=[
+            pdk.Layer(
+               'HexagonLayer',
+               data=chart_data,
+               get_position='[lon, lat]',
+               radius=200,
+               elevation_scale=4,
+               elevation_range=[0, 1000],
+               pickable=True,
+               extruded=True,
+            ),
+            pdk.Layer(
+                'ScatterplotLayer',
+                data=chart_data,
+                get_position='[lon, lat]',
+                get_color='[200, 30, 0, 160]',
+                get_radius=200,
+            ),
+        ],
+    ))
 
 
 
