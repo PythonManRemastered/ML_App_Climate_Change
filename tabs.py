4a51309a3b4da7f7c7c1283e0d8bee821ce52ed3
PythonManRemastered <109542449+PythonManRemastered@users.noreply.github.com>
2024-05-29 14:47:03 +0530

    Update tabs.py

--- tabs.py
+++ tabs.py
@@ -63,39 +63,6 @@ with tab2:
                 """                
 
 )
-       
-    chart_data = pd.DataFrame(
-       np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-       columns=['lat', 'lon'])
-    
-    st.pydeck_chart(pdk.Deck(
-        map_style=None,
-        initial_view_state=pdk.ViewState(
-            latitude=37.76,
-            longitude=-122.4,
-            zoom=11,
-            pitch=50,
-        ),
-        layers=[
-            pdk.Layer(
-               'HexagonLayer',
-               data=chart_data,
-               get_position='[lon, lat]',
-               radius=200,
-               elevation_scale=4,
-               elevation_range=[0, 1000],
-               pickable=True,
-               extruded=True,
-            ),
-            pdk.Layer(
-                'ScatterplotLayer',
-                data=chart_data,
-                get_position='[lon, lat]',
-                get_color='[200, 30, 0, 160]',
-                get_radius=200,
-            ),
-        ],
-    ))
 
 
 
@@ -151,6 +118,41 @@ with tab5:
     expander.write("""Several other protocols have also been put into place to combat climate emissions, notably: **The Kyoto Protocol (and the Doha amendment), the Clean Air Act,
                    and the Clean Water Act**
                    """)
+    st.divider()
+    
+    chart_data = pd.DataFrame(
+       np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+       columns=['lat', 'lon'])
+    
+    st.pydeck_chart(pdk.Deck(
+        map_style=None,
+        initial_view_state=pdk.ViewState(
+            latitude=37.76,
+            longitude=-122.4,
+            zoom=11,
+            pitch=50,
+        ),
+        layers=[
+            pdk.Layer(
+               'HexagonLayer',
+               data=chart_data,
+               get_position='[lon, lat]',
+               radius=200,
+               elevation_scale=4,
+               elevation_range=[0, 1000],
+               pickable=True,
+               extruded=True,
+            ),
+            pdk.Layer(
+                'ScatterplotLayer',
+                data=chart_data,
+                get_position='[lon, lat]',
+                get_color='[200, 30, 0, 160]',
+                get_radius=200,
+            ),
+        ],
+    ))
+    st.warning('This data is purely theoretical, and serves simply to show the typical behaviour of pollution levels in a qualitative, not quantitative manner', icon="⚠️")
 
 
 with tab6:
